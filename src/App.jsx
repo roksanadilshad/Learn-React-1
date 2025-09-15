a6871fd7df250d532878e251042b77f814dd3768
Roksana Dilshad <roksanadilshad@gmail.com>
2025-09-16 00:40:11 +0600

    learned click items

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  function handle(){
+    alert("I'm Clicked");
+  }
+  const handleClick = () => {
+    alert('Again clicked')
+  }
+  const handleClick2 = (num) => {
+    const number = num + 4;
+    alert(number)
+  }
+
+  return (
+    <>
+      
+      <h3>Vite + React</h3>
+      <button onClick={handle}>Click Me</button>
+      <button onClick={() => {alert('You are beautiful')}}>Click can change you</button>
+      <button onClick={handleClick}>Wanna click more?</button>
+      <button onClick={() => handleClick2(14)}>Click After</button>
+      
+    </>
+  )
+}
+
+
+export default App
